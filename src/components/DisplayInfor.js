ec63eae348d9b76696ed94c8b02f517a7027335a
VuNQ9 <vunqf8@gmail.com>
2022-09-29 16:55:11 +0700

    video 27

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./DisplayInfor.scss";
 class DisplayInfor extends React.Component {
 
     state = {
@@ -17,7 +17,7 @@ class DisplayInfor extends React.Component {
         console.log(listUsers);
         // props => viet tat properties
         return (
-            <div>
+            <div className="Display-infor-container">
                 <div>
                     <span onClick={() => { this.handleShowHide() }}>
                         {this.state.isShowListUser === true ? "Hide list users: " : "Show list users: "}
