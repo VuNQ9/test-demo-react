74ea64fbca28a9364931b5a10484ed59b14cc96b
VuNQ9 <vunqf8@gmail.com>
2022-10-21 16:41:48 +0700

    video 62

--- src/components/Admin/Content/TableUser.js
+++ src/components/Admin/Content/TableUser.js
@@ -1,35 +1,14 @@
-import { useEffect, useState } from "react";
-import { getAllUsers } from "../../../services/apiServices"
+
 const TableUser = (props) => {
 
-    const [listUsers, setListUsers] = useState([
-        {
-            "id": 5,
-            "username": "eric",
-            "email": "hoidanit@gmail.com",
-            "role": "USER",
-            "image": ""
-        }
-    ]);
-
-    //componentDidMount
-    useEffect(() => {
-        fetchLisUsers()
-    }, []);
-
-    const fetchLisUsers = async () => {
-        let res = await getAllUsers();
-        console.log(res);
-        if (res.EC === 0) {
-            setListUsers(res.DT);
-        }
-    }
+    const { listUsers } = props;
+    // const listUsers = props.listUsers;
     return (
         <>
             <table className="table table-hover table-bordered">
                 <thead>
                     <tr>
-                        <th scope="col">No</th>
+                        <th scope="col">ID</th>
                         <th scope="col">Username</th>
                         <th scope="col">Email</th>
                         <th scope="col">Role</th>
@@ -41,7 +20,7 @@ const TableUser = (props) => {
                         listUsers.map((item, index) => {
                             return (
                                 <tr key={`table-users-${index}`}>
-                                    <td>{index + 1}</td>
+                                    <td>{item.id}</td>
                                     <td>{item.username}</td>
                                     <td>{item.email}</td>
                                     <td>{item.role}</td>
