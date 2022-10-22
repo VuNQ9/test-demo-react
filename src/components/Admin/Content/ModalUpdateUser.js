b7b60ba75c72a83e94f2274665b47742f28c41b4
VuNQ9 <vunqf8@gmail.com>
2022-10-23 05:45:14 +0700

    video 64

--- src/components/Admin/Content/ModalUpdateUser.js
+++ src/components/Admin/Content/ModalUpdateUser.js
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FcPlus } from 'react-icons/fc';
 import { toast } from 'react-toastify';
-import { postCreateNewUser } from "../../../services/apiServices";
+import { putUpdateUser } from "../../../services/apiServices";
 import _ from "lodash";
 const FormData = require('form-data');
 const ModalUpdateUser = (props) => {
@@ -17,6 +17,7 @@ const ModalUpdateUser = (props) => {
         setRole("");
         setImage("");
         setPreviewImage("");
+        props.resetUpdateData();
     }
 
     const [email, setEmail] = useState("");
@@ -66,12 +67,7 @@ const ModalUpdateUser = (props) => {
             return;
         }
 
-        if (!password) {
-            toast.error("Invalid password");
-            return;
-        }
-
-        let data = await postCreateNewUser(email, password, username, role, image);
+        let data = await putUpdateUser(dataUpdate.id, username, role, image);
         if (data && data.EC === 0) {
             toast.success(data.EM);
             handleClose();
